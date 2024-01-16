8ae687cab532f08a0f0f8d0940b0041bf19bf993
aklab <aklab@aklabs-MacBook-Pro.local>
2024-01-16 17:55:58 +0900

    disabled noice autopair and surround motion

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,2 +1,7 @@
-M = {}
+M = {
+  { "folke/noice.nvim", enabled = false },
+  --{ "folke/flash.nvim", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+  { "echasnovski/mini.surround", enabled = false },
+}
 return M
