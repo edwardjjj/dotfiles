2851e641d2242e8a21ce9bb7cc8e1e3196bfc57c
aklab <aklab@aklabs-MacBook-Pro.local>
2024-01-16 17:54:52 +0900

    added neogen config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -12,3 +12,28 @@ map({ "n" }, "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { desc = "window left", silen
 map({ "n" }, "<C-l>", "<cmd>TmuxNavigateRight<CR>", { desc = "window right", silent = true })
 map({ "n" }, "<C-j>", "<cmd>TmuxNavigateDown<CR>", { desc = "window down", silent = true })
 map({ "n" }, "<C-k>", "<cmd>TmuxNavigateUp<CR>", { desc = "window up", silent = true })
+
+map(
+  { "n" },
+  "<leader>nn",
+  "<cmd>Neogen func<CR>",
+  { desc = "Generate documentation for function", silent = true, noremap = true }
+)
+map(
+  { "n" },
+  "<leader>nc",
+  "<cmd>Neogen class<CR>",
+  { desc = "Generate documentation for class", silent = true, noremap = true }
+)
+map(
+  { "n" },
+  "<leader>nf",
+  "<cmd>Neogen file<CR>",
+  { desc = "Generate documentation for file", silent = true, noremap = true }
+)
+map(
+  { "n" },
+  "<leader>nt",
+  "<cmd>Neogen type<CR>",
+  { desc = "Generate documentation for type", silent = true, noremap = true }
+)
---
+++ nvim/.config/nvim/lua/plugins/neogen.lua
@@ -0,0 +1,11 @@
+return {
+  "danymat/neogen",
+  dependencies = "nvim-treesitter/nvim-treesitter",
+  config = function()
+    require("neogen").setup({
+      snippet_engine = "luasnip",
+    })
+  end,
+  -- Uncomment next line if you want to follow only stable versions
+  -- version = "*"
+}
