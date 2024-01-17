e87cdf8768c292dd9eb39e585acafbfc30117254
edwardjjj <edward.jwjiang@gmail.com>
2024-01-17 23:46:43 +0900

    changed keymap

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,12 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
+map({ "i", "v" }, "jk", "<esc>", { desc = "Fast escape", noremap = true })
 map({ "v" }, "<leader>/", "gc", { desc = "Toggle Comment", remap = true })
-
 map({ "n" }, "<leader>/", "gcc", { desc = "Toggle Comment", remap = true })
 
-map({ "n", "o", "x" }, "gs", "gz", { desc = "Mini Surround", remap = true })
-
+-- Tmux Navigate keymaps
 map({ "n" }, "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { desc = "window left", silent = true })
 map({ "n" }, "<C-l>", "<cmd>TmuxNavigateRight<CR>", { desc = "window right", silent = true })
 map({ "n" }, "<C-j>", "<cmd>TmuxNavigateDown<CR>", { desc = "window down", silent = true })
